ebdf879f3c1d98e1ca22ab061cf8eb05694d2216
avigayile613 <avigayile613@gmail.com>
2025-11-25 22:29:24 -0500

    some corrections

--- 0 database.sql
+++ 0 database.sql
@@ -1,4 +1,3 @@
 create database FinthumDB
 go
-using FinthumDB
-finthum 
+use FinthumDB
--- 1 table.sql
+++ 1 table.sql
@@ -1,8 +1,10 @@
-use FunthumDB
+use FinthumDB
+go
+drop table if exists body
 go
 create table dbo.body(id int not null identity primary key, 
     bodypart varchar(100) not null, 
     num int not null
     )
     
-)
+   
