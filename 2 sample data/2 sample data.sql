da534ec55303e42204b6cd8c8fc16442efc1e1ee
avigayile613 <avigayile613@gmail.com>
2025-11-25 23:02:29 -0500

    Update 2 sample data.sql

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -4,9 +4,9 @@ insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
-union select 'mouth', 2
+union select 'mouh', 2
 union select 'teeth', 32
-union select 'toungue', 1
+union select 'tougue', 1
 
 insert body(bodypart)
 select 'hair'
