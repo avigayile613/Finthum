4eb1401dd8a3aa026a792d106d8325950257d72e
avigayile613 <avigayile613@gmail.com>
2025-11-25 22:43:43 -0500

    made num nullable and added unique constraint

--- 1 tables/1 table.sql
+++ 1 tables/1 table.sql
@@ -3,8 +3,8 @@ go
 drop table if exists body
 go
 create table dbo.body(id int not null identity primary key, 
-    bodypart varchar(100) not null, 
-    num int not null
+    bodypart varchar(100) not null constraint u_body_bodypart unique, 
+    num int null
     )
     
    
--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -1,3 +1,5 @@
+delete body
+
 insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
@@ -5,3 +7,9 @@ union select 'nose', 1
 union select 'mouth', 2
 union select 'teeth', 32
 union select 'toungue', 1
+
+insert body(bodypart)
+select 'hair'
+
+
+select * from body
