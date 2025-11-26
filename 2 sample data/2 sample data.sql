cedbd9c6959e8e46f7606669c0e3d0961617a54d
avigayile613 <avigayile613@gmail.com>
2025-11-25 23:26:29 -0500

    code review see comments in files

--- 2 sample data/2 sample data.sql
+++ 2 sample data/2 sample data.sql
@@ -1,10 +1,12 @@
+--AE please populate sample data with planets to match new schema.
+
 delete body
 
 insert body(bodypart, num)
 select 'eyes', 2
 union select 'ears', 2
 union select 'nose', 1
-union select 'mouh', 2
+union select 'mouh', 2 --AE was this change a mistake??
 union select 'teeth', 32
 union select 'tougue', 1
 
--- 2 sample data/planets.sql
+++ 2 sample data/planets.sql
@@ -1,3 +1,4 @@
+--this probably belongs in sample data.
 earth
 mars
 jupiter
